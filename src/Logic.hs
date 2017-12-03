27ff110a7bef90254dbbdb19ba5b78d4cc37d2a7
seoular <34080384+seoular@users.noreply.github.com>
2017-12-02 20:45:15 -0500

    Update Logic.hs
    Finished game logic except for randomness

--- src/Logic.hs
+++ src/Logic.hs
@@ -4,14 +4,17 @@ import Data.Maybe
 import Data.List
 import Prelude
 
+import qualified System.Random    as R
+
+
 -- import System.IO (stdin, hReady)
 -- import System.IO (stdin, hSetEcho)
 -- import Control.Monad (when)
 -- import System.Exit
 
 
-import Control.Monad.IO.Class
-import System.Console.Haskeline
+-- import Control.Monad.IO.Class
+-- import System.Console.Haskeline
 -- import qualified System.Random
 
 type Tile = Maybe Int
@@ -48,7 +51,8 @@ printTile t = case t of
 
 colorRow :: [Tile] -> IO ()
 colorRow [] = putStrLn ""
-colorRow (x:xs) = do putStr $ "\x1b[32m" ++ (printTile x) ++ " | "
+colorRow (x:xs) = do putStr $ (printTile x) ++ "   "
+-- colorRow (x:xs) = do putStr $ "\x1b[32m" ++ (printTile x) ++ " | "
                      colorRow xs
 
 colorGrid :: Grid -> IO ()
@@ -56,6 +60,152 @@ colorGrid g = case g of
  [] -> putStrLn ""
  x:xs -> do colorRow x
             colorGrid xs
+
+-- updateGrid :: Grid -> Grid
+-- updateGrid g = do
+
+scoreGrid :: Grid -> Int -> Int
+scoreGrid [] n = n
+scoreGrid (x:xs) n = do 
+                     let temp = scoreRow x 0
+                     if temp > n then scoreGrid xs temp
+                     else scoreGrid xs n
+
+
+scoreRow :: [Tile] -> Int -> Int
+scoreRow [] n = n
+scoreRow (x:xs) n = case x of
+ Nothing -> scoreRow xs n
+ Just a -> if a > n then scoreRow xs a
+           else scoreRow xs n
+
+-- terminalstateCheck :: Grid -> Int
+-- terminalstateCheck [] = 0
+-- terminalStateCheck g = do
+--  let score = scoreGrid g
+--  case score of
+--   0 -> 
+
+
+-- input should be in the form of a filled board
+stuckCheck :: Grid -> Bool
+stuckCheck g = do
+  case g of
+     [[Just a,Just b, Just c, Just d],
+      [Just e,Just f, Just g, Just h],
+      [Just i,Just j, Just k, Just l],
+      [Just m,Just n, Just o, Just p]] -> if (a == b || b == c || c == d ||
+                                              e == f || f == g || g == h ||
+                                              i == j || j == k || k == l ||
+                                              m == n || n == o || o == p ||
+                                              a == e || e == i || i == m ||
+                                              b == f || f == j || j == n ||
+                                              c == g || g == k || k == o ||
+                                              d == h || h == l || l == p) then False else True
+  -- let other = map reverse $ leftGrid (map reverse g)
+  -- let num = 0
+  -- case g of
+  --   (x:xs) -> (stuckRow x) + stuckRow xs
+  --               _ -> 5
+
+       
+                                          
+-- stuckRow :: [Tile] -> Int
+-- stuckRow row = do
+--   case row of
+--     [Just a, Just b, Just c, Just d] -> if (a == b || b == c || c == d) then 1 else 0
+checkFull :: Grid -> Bool
+checkFull g = case g of 
+  [] -> True
+  (x:xs) -> checkRowExists x && checkFull xs
+
+checkRowExists :: [Tile] -> Bool
+checkRowExists r = case r of
+  [] -> True
+  (x:xs) -> checkTileExists x && checkRowExists xs
+
+checkTileExists :: Tile -> Bool
+checkTileExists t = case t of
+  Nothing -> False
+  _ -> True
+
+-- insertRandomTile :: R.RandomGen rand => rand -> Grid -> Grid
+-- insertRandomTile rand g = case g of  
+--    (x:xs) -> if checkRowExists x then x:(insertRandomTile rand xs) 
+--              else do
+--                 if checkFull xs then (insertRandomTileInRow rand x):xs
+--                 else do
+--                    let (num, rand') = R.randomR (1,10) rand
+--                    if (num::Int) <= 6 then x:(insertRandomTile rand' xs)
+--                    else (insertRandomTileInRow rand' x):xs
+
+-- insertRandomTileInRow :: R.RandomGen rand => rand -> [Tile] -> [Tile]
+-- insertRandomTileInRow rand r = case r of
+--    (x:xs) -> if checkTileExists x then x:(insertRandomTileInRow rand xs)
+--             else do 
+--                if checkRowExists xs then (makeRandomTile rand):xs
+--                else do
+--                    let (num, rand') = R.randomR (1,10) rand
+--                    if (num::Int) <= 6 then x:(insertRandomTileInRow rand' xs)
+--                     else (makeRandomTile rand'):xs
+insertRandomTile :: Grid -> Grid
+insertRandomTile g = case g of 
+   (x:xs) -> if checkRowExists x then x:(insertRandomTile xs) 
+             else (insertRandomTileInRow  x):xs
+
+insertRandomTileInRow :: [Tile] -> [Tile]
+insertRandomTileInRow r = case r of
+   (x:xs) -> if checkTileExists x then x:(insertRandomTileInRow xs)
+             else (makeRandomTile):xs
+              
+            
+makeRandomTile :: Tile
+makeRandomTile = Just 2
+-- makeRandomTile = do
+--    let (num, rand') = R.randomR (1,10) rand
+--    if (num::Int) <= 9 then Just 2 else Just 4
+   
+
+
+
+  
+
+primaryLoop :: Grid -> IO ()
+primaryLoop g = do
+    colorGrid g
+    let num = scoreGrid g 0
+    if (checkFull g) && (stuckCheck g) then do       
+        putStrLn "Stuck. Your score is: "
+        print num
+        return ()     
+    else do
+        case num of 
+            2048 -> do
+               putStrLn "YOU WIN"
+               return ()
+            _ -> do
+              
+               -- print num
+               c <- getLine
+               case c of
+                    "w" -> primaryLoop  $ insertRandomTile $ transpose $ leftGrid $ transpose g
+                    "s" -> primaryLoop  $ insertRandomTile $ transpose $ map reverse $ leftGrid $ map reverse $ transpose g
+                    "d" -> primaryLoop  $ insertRandomTile $ map reverse $ leftGrid (map reverse g)
+                    "a" -> primaryLoop  $ insertRandomTile $ leftGrid g
+                    _   -> return ()
+
+main = do
+
+    let g =        [[Just 2, Just 2, Nothing, Nothing],
+                    [Nothing, Nothing, Nothing, Nothing],
+                    [Nothing, Nothing, Nothing, Nothing],
+                    [Nothing, Nothing, Nothing, Nothing]]
+    -- print $ stuckCheck g
+    primaryLoop g
+    -- primaryLoop g
+ 
+    
+
 
 
 
@@ -93,4 +243,3 @@ colorGrid g = case g of
 --   39 -> map reverse $ leftGrid (map reverse g) -- arrow right
 --   38 -> transpose $ leftGrid $ transpose g     -- arrow up
 --   40 -> transpose $ map reverse $ leftGrid $ map reverse $ transpose g -- arrow down
-
